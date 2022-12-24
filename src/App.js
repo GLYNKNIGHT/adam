47848b2a4afbd5b436a774be14b114a858a487dd
glynk <glyn_knight@hotmail.com>
2022-12-24 22:00:05 +0000

    a

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         
     <br></br>
-        <video width="450" height="auto" autoPlay muted loop>
+        <video width="95%" height="auto" autoPlay muted loop playsInline>
             <source src={video} type="video/mp4" />
         </video>
       <br></br>
@@ -22,9 +22,10 @@ function App() {
         </a>
 <br></br>
         <div className="google-map-code">
-        <iframe title="google" src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d90251.13875234386!2d-2.9011073122203412!3d54.592328905663514!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1swaterside%20farm%20campsite!5e1!3m2!1sen!2suk!4v1671800374103!5m2!1sen!2suk" width="500px" height="300px" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe title="google" src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d90251.13875234386!2d-2.9011073122203412!3d54.592328905663514!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1swaterside%20farm%20campsite!5e1!3m2!1sen!2suk!4v1671800374103!5m2!1sen!2suk" width="370px" height="300px" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
          
         </div>
+        <br></br>
       </header>
     </div>
   );
